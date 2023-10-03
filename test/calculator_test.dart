8479e84b6fd0895d309f68ff541594acc1b5c434
JuanDQuinteroBF <juan.quintero@blankfactor.com>
2023-10-02 23:36:11 -0500

    First commit

---
+++ test/calculator_test.dart
@@ -0,0 +1,28 @@
+// test/my_test.dart
+import 'package:test/test.dart';
+import 'package:my_project_name/calculator.dart'; // Asegúrate de importar tu código fuente
+void main() {
+  group('Counter', () {
+    test('value should start at 0', () {
+      expect(Counter().value, 0);
+    });
+
+    test('value should be incremented', () {
+      //Arrange
+      final counter = Counter();
+      //Act
+      counter.increment();
+      //Assert 
+      expect(counter.value, 1);
+    });
+
+    test('value should be decremented', () {
+      //Arrange
+      final counter = Counter();
+      //Act
+      counter.decrement();
+      //Assert 
+      expect(counter.value, -1);
+    });
+  });
+}
